b79defad84369ce0369e7667efe83f42ab89d4aa
Moho2023 <90466441+Moho2023@users.noreply.github.com>
2023-03-02 09:14:22 -0500

    login view

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+app.use(express.json()); //Used to parse JSON bodies (needed for POST requests)
+app.use(express.urlencoded());
+app.use(express.static('public')); //specify location of static assests
+app.set('views', __dirname + '/views'); //specify location of templates
+app.set('view engine', 'ejs'); //specify templating library
+
+app.get('/', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("index");
+});
+
+app.get('/login', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("login");
+});
+
+//..............Start the server...............................//
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server started at http://localhost:'+port+'.')
+});
