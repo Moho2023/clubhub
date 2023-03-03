45dde5b26c19307052224eb9b93a3061d124267b
Moho2023 <90466441+Moho2023@users.noreply.github.com>
2023-03-03 12:08:03 -0500

    organized controllers

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const ejs = require('ejs');
+const methodOverride = require('method-override');
 
 const app = express();
 app.use(express.json()); //Used to parse JSON bodies (needed for POST requests)
 app.use(express.urlencoded());
+app.use(methodOverride('_method'));
 app.use(express.static('public')); //specify location of static assests
 app.set('views', __dirname + '/views'); //specify location of templates
 app.set('view engine', 'ejs'); //specify templating library
 
-app.get('/', function(request, response) {
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("index");
+//app.use(require('./controllers/auth.js'))
+//app.use(require('./controllers/admin_controller.js'))
+//app.use(require('./controllers/club_controller.js'))
+//app.use(require('./controllers/manage_controller.js'))
+app.use(require('./controllers/index.js'))
+
+app.use("", function(request, response) {
+  response.redirect('/error?code=400');
 });
 
-app.get('/login', function(request, response) {
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("login");
-});
 
 //..............Start the server...............................//
 const port = process.env.PORT || 3000;
