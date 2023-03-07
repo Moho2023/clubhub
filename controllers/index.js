9b6c49c693b82bf64dd86da65af624aa8b20cd53
Moho2023 <90466441+Moho2023@users.noreply.github.com>
2023-03-07 09:01:32 -0500

    kaenufiehs

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 router = express.Router();
 
-
+ 
 router.get('/', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
