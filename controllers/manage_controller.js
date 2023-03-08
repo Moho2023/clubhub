53bfcb75e9f732a7a03957d92c61547c5c637f2d
Moho2023 <90466441+Moho2023@users.noreply.github.com>
2023-03-07 23:11:36 -0500

    manage

--- controllers/manage_controller.js
+++ controllers/manage_controller.js
@@ -1,5 +1,20 @@
 const express = require('express');
 router = express.Router();
 
+function loggedIn(request, response, next) {
+  if (request.user) {
+    next();
+  } else {
+    response.redirect('/denied');
+  }
+}
+
+router.get('/manage', loggedIn, function(request, response){
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("management", {
+    user: request.user
+  });
+})
 
 module.exports = router
